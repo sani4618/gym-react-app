ae7d7dcccba7419f92872e1316d27421d1ad34c2
sani4618 <sanijoseph2018@gmail.com>
2023-09-03 17:01:31 +0530

    home is updated

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                         <div className="row">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <div id="carouselExampleCaptions" className="carousel slide">
