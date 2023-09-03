60214fc4f65182c07056192f78e803cfda222daf
sani4618 <sanijoseph2018@gmail.com>
2023-09-03 18:34:17 +0530

    home about is updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 ">
+
+                            <p><b><i>ABOUT US</i></b></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita amet voluptas cum quaerat cupiditate aut id veniam, doloremque pariatur sequi ratione fugit doloribus eveniet iusto, aspernatur dicta voluptatum? Officia, adipisci!</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
